cefe390e248ac4c9081bc688efa172ce3d708495
Dido <Dido@qq.com>
2022-09-14 19:48:31 +0800

    lodash

---
+++ lodash/ashley_dido-lodash.js
@@ -0,0 +1,15 @@
+var dido = {
+  chunk: function (array, size = 1) {
+    let arr = [];
+    let arr1 = [];
+    for (let i = 0; i < array.length; i++) {
+      arr1.push(array[i]);
+      if (arr1.length == size) {
+        arr.push(arr1);
+        arr1 = [];
+      }
+    }
+    arr.push(arr1);
+    return arr;
+  },
+};
