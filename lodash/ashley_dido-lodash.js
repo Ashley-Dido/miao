131d84acab1074c3676926f7c5255528d554e626
Dido <Dido@qq.com>
2022-09-16 11:14:00 +0800

    5

--- lodash/ashley_dido-lodash.js
+++ lodash/ashley_dido-lodash.js
@@ -1,5 +1,5 @@
-var ashley_dido = {
-  chunk: function (array, size = 1) {
+var ashley_dido = (function () {
+  function chunk(array, size = 1) {
     let arr = [];
     let arr1 = [];
     for (let i = 0; i < array.length; i++) {
@@ -13,9 +13,9 @@ var ashley_dido = {
       arr.push(arr1);
     }
     return arr;
-  },
+  }
 
-  compact: function (array) {
+  function compact(array) {
     let filter = [];
     for (let i = 0; i < array.length; i++) {
       if (array[i]) {
@@ -23,15 +23,92 @@ var ashley_dido = {
       }
     }
     return filter;
-  },
+  }
 
-  difference: function (array, values) {
-    let filter = [];
-    for (let key in array) {
-      if (!(key in values)) {
-        filter.push(key);
+  function fill(array, value, start = 0, end = array.length) {
+    for (let i = start; i < end; i++) {
+      array[i] = value;
+    }
+    return array;
+  }
+
+  function drop(array, n = 1) {
+    for (let i = 0; i < n; i++) {
+      if (i > 0) {
+        array.shift();
       }
     }
-    return filter;
-  },
-};
+    return array;
+  }
+
+  function findIndex(array, f = (it) => it, startIndex = 0) {
+    for (let i = startIndex; i < array.length; i++) {
+      if (f(array[i])) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  function findLastIndex(
+    array,
+    f = (it) => it,
+    startIndex = array.lenngth - 1
+  ) {
+    for (let i = startIndex; i >= 0; i--) {
+      if (f(array[i])) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  return {
+    chunk,
+    compact,
+    fill,
+    drop,
+    findIndex,
+    findLastIndex,
+    // flatten,
+    // flattenDeep,
+    // flattenDepth,
+    // fromPairs,
+    // head,
+    // indexOf,
+    // lastIndexOf,
+    // initial,
+    // join,
+    // last,
+    // pull,
+    // reverse,
+    // every,
+    // some,
+    // countBy,
+    // groupBy,
+    // keyBy,
+    // forEach,
+    // map,
+    // filter,
+    // reduce,
+    // reduceRight,
+    // size,
+    // sortBy,
+    // sample,
+    // isUndefined,
+    // isNull,
+    // isNil,
+    // max,
+    // min,
+    // maxBy,
+    // minBy,
+    // round,
+    // sumBy,
+    // flagMap,
+    // flatMapDepth,
+    // get,
+    // has,
+    // mapKeys,
+    // mapValues,
+  };
+})();
